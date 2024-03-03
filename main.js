d940a86b1edf41ef933cac8bd003a76ba3b34f22
Jeyfrem <153380072+dieGeisterhand@users.noreply.github.com>
2024-03-03 10:36:45 -0500

    looking better

--- main.js
+++ main.js
@@ -17,85 +17,106 @@ consoleColor.style.setProperty('--body-color', colors[randomColor]);
 
 const action = document.getElementById("left-button");
 const option = document.getElementById("right-button");
-const titleText = document.getElementById("main-text");
-const aboutText = document.getElementById("about-text");
+
+const mainText = document.getElementById("main-text");
+
 const userPokemonImg = document.getElementById("image-container-bottom");
 const opponentPokemonImg = document.getElementById("image-container-top");
+
 const userName = document.getElementById("your-name");
 const userHpTitle = document.getElementById("hp-text");
-const computerHpTitle = document.getElementById("computer-hp-text");
-const computerName = document.getElementById("computer-name");
-const currentOpponentHp = document.getElementById("computer-current-hp");
 const currentUserHp = document.getElementById("user-current-hp");
-const divisionBottom = document.getElementById("health-division-bottom");
-const divisionTop = document.getElementById("health-division-top");
 const userMaxHp = document.getElementById("user-max-hp");
+const divisionBottom = document.getElementById("health-division-bottom");
+
+const computerName = document.getElementById("computer-name");
+const computerHpTitle = document.getElementById("computer-hp-text");
+const currentOpponentHp = document.getElementById("computer-current-hp");
 const computerMaxHp = document.getElementById("computer-max-hp");
+const divisionTop = document.getElementById("health-division-top");
 
+const pokemon = [
+  {
+    name: "PIDGEY",
+    sprite: ["./assets/pokemon-sprites/pidgey-back.png", "./assets/pokemon-sprites/pidgey-front.png"],
+    base_hp: 9,
+    moves: ["GUST", "SAND ATTACK", "QUICK ATTACK", "TACKLE"]
+  },
+  {
+    name: "RATTATA",
+    sprite: ["./assets/pokemon-sprites/rattata-back.png", "./assets/pokemon-sprites/rattata-front.png"],
+    base_hp: 8,
+    moves: ["QUICK ATTACK", "BITE", "TAIL WHIP", "TACKLE"]
+  },
+  {
+    name: "MAGIKARP",
+    sprite: ["./assets/pokemon-sprites/magikarp-back.png", "./assets/pokemon-sprites/magikarp-front.png"],
+    base_hp: 20,
+    moves: ["SPLASH", "SPLASH", "SPLASH", "HYPER BEAM"],
+  }
+];
 
+const gameScreen = [
+  {
+    name: "main screen",
+    main_text: "POKÉMON BATTLE",
+    button_text: ["PLAY", "ABOUT"],
+    button_functions: [play, about]
+  },
+  {
+    name: "about",
+    main_text: "This is POKÉMON BATTLE. Use the ATTACK button to make your POKÉMON use a random attack. With ITEM, you can use a random ITEM to heal them. Your opponent, your POKÉMON and their movesets are randomly chosen. See how well you fare against the COMPUTER!",
+    button_text: ["", "RETURN"],
+    button_functions: [null, mainScreen]
+  },
+  {
+    name: "game",
+    main_text: [`What would ${pokemon} do?`, `${pokemon} used ${pokemon}!`, `You used ${pokemon}`],
+    button_text: ["ATTACK", "ITEM"],
+    button_functions: [attack, item]
+  }
+]
 
 
 function mainScreen() {
-  titleText.innerText = "POKÉMON BATTLE";
+  mainText.innerText = gameScreen[0]["main_text"];
 
-  action.innerText = "PLAY";
+  action.innerText = gameScreen[0]["button_text"][0];
   action.setAttribute("onclick", "play()");
 
-  option.innerText = "ABOUT";
+  option.innerText = gameScreen[0]["button_text"][1];
   option.setAttribute("onclick", "about()");
   
-  aboutText.innerText = "";
 };
 
 function about() {
-  titleText.innerText = "";
-  aboutText.innerText = "This is POKÉMON BATTLE. Use the ATTACK button to make your POKÉMON use a random attack. With ITEM, you can use a random ITEM to heal them. Your opponent, your POKÉMON and their movesets are randomly chosen. See how well you fare against the COMPUTER!";
-  
-  action.innerText = "";
+  mainText.innerText = gameScreen[1]["main_text"];
+  action.innerText = gameScreen[1]["button_text"][0];
   action.removeAttribute("onclick");
 
-  option.innerText = "RETURN";
+  option.innerText = gameScreen[1]["button_text"][1];
   option.setAttribute("onclick", "mainScreen()");
 };
 
 function play() {
-  titleText.innerText = "What would PIDGEY do now?";
-
-  action.innerText = "ATTACK";
+  action.innerText = gameScreen[2]["button_text"][0];
   action.setAttribute("onclick", "attack()");
 
-  option.innerText = "ITEM";
-  option.setAttribute("onclick", "item()");
+  option.innerText = gameScreen[2]["button_text"][1];
+  option.setAttribute("onclick", "");
 
-  userPokemonImg.setAttribute("src", "./1.png");
-  opponentPokemonImg.setAttribute("src", "./2.gif")
+  const randomNumberUser = Math.floor(Math.random() * pokemon.length);
+  const randomNumberOpponent = Math.floor(Math.random() * pokemon.length);
 
-  userName.innerText = "PIDGEY";
-  computerName.innerText = "NIDORAN♂";
+  userPokemonImg.setAttribute("src", pokemon[randomNumberUser]["sprite"][0]);
+  opponentPokemonImg.setAttribute("src", pokemon[randomNumberOpponent]["sprite"][1]);
 
-  userHpTitle.innerText = "HP:";
-  computerHpTitle.innerText = "HP:";
+  mainText.innerText = `What would ${pokemon[randomNumberUser]["name"]} do?`
+}
 
-  // REVISE THE CODE BELOW WHEN YOU ADD POKEMONS FROM AN OBJECT LIST
-  currentUserHp.innerText = 19;
-  currentOpponentHp.innerText = 21;
 
-  divisionTop.innerText = "/";
-  divisionBottom.innerText = "/";
-
-  userMaxHp.innerText = "19";
-  computerMaxHp.innerText = "21";
-
-  // YES, THE ONES ABOVE
-
-};
-
-function attack() {
-  titleText.innerText = "Work in progress!"
-  return;
-};
-
-function item(){};
+function attack() {}
+function item() {}
 
 window.onload = function() {
   mainScreen();
